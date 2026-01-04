30994d376f4d04e564b14f41667583a2b52fddca
Ernest Solomon <marveldad2@gmail.com>
2026-01-04 03:03:15 +0100

    Writing a simple python script that creates the database alx_book_store in my MySQL server

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,32 +1,24 @@
 #!/usr/bin/python3
 import mysql.connector
-from mysql.connector import Error
 
 def create_database():
     connection = None
     try:
-        # CONNECT TO MYSQL SERVER
         connection = mysql.connector.connect(
             host="localhost",
             user="marveldad2@gmail.com",
-            password="Letmein141@"  # UPDATE IF YOUR MYSQL HAS A PASSWORD
+            password="Letmein141@"
         )
 
-        if connection.is_connected():
-            cursor = connection.cursor()
+        cursor = connection.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
 
-            # CREATE DATABASE IF NOT EXISTS
-            cursor.execute(
-                "CREATE DATABASE IF NOT EXISTS alx_book_store"
-            )
+        print("Database 'alx_book_store' created successfully!")
 
-            print("Database 'alx_book_store' created successfully!")
-
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error while connecting to MySQL: {e}")
 
     finally:
-        # CLOSE CONNECTION
         if connection is not None and connection.is_connected():
             cursor.close()
             connection.close()
